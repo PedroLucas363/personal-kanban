1b9c525c7b37a82eddb1f216ef636346dd980698
Pedro Lucas Rando <pedrolucas363@gmail.com>
2023-03-22 19:58:27 -0300

    chore: add colors variables on ts file

---
+++ src/colors.ts
@@ -0,0 +1,13 @@
+export default {
+  darkBrown: "var(--dark-brown)",
+  brown: "var(--brown)",
+  darkPurple: "var(--dark-purple)",
+  purple: "var(--purple)",
+  lightPurple: "var(--light-purple)",
+  darkGray: "var(--dark-gray)",
+  gray: "var(--gray)",
+  lightGray: "var(--light-gray)",
+  lighterGray: "var(--lighter-gray)",
+  white: "var(--white)",
+  offWhite: "var(--off-white)",
+};
