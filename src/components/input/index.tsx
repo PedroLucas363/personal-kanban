6070f4d222de0bc1543205f4f22064214fd6b05a
Pedro Lucas Rando <pedrolucas363@gmail.com>
2023-03-23 08:27:18 -0300

    chore: add input component

---
+++ src/components/input/index.tsx
@@ -0,0 +1,24 @@
+import React, { InputHTMLAttributes } from "react";
+
+import styles from "./index.module.css";
+
+type Props = InputHTMLAttributes<HTMLElement> & {
+  label?: string;
+  icon?: React.ReactElement;
+  iconPosition?: "left" | "right";
+};
+
+function Input({ label, icon, iconPosition, ...props }: Props) {
+  return (
+    <div className={styles.inputContainer}>
+      {label}
+      <div className={styles.input}>
+        {icon && iconPosition === "left" ? <i>{icon}</i> : null}
+        <input {...props} />
+        {icon && iconPosition === "right" ? <i>{icon}</i> : null}
+      </div>
+    </div>
+  );
+}
+
+export default Input;
