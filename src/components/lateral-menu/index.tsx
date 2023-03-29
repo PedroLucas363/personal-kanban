9fa9205e6b1249a75ce33ce2ad2abd7afdf00604
Pedro Lucas Rando <pedrolucas363@gmail.com>
2023-03-28 22:39:37 -0300

    feat: changing the logo of project to a animated personal logo

--- src/components/lateral-menu/index.tsx
+++ src/components/lateral-menu/index.tsx
@@ -6,7 +6,6 @@ import {
 } from "react-icons/io5";
 
 import Button from "../button";
-import Logo from "../../assets/logo.png";
 
 import styles from "./index.module.css";
 import colors from "../../colors";
@@ -25,7 +24,24 @@ function LateralMenu({ currentTab, onTabChange }: Props) {
 
   return (
     <section className={styles.container}>
-      <img src={Logo} alt="imagem do logo" className={styles.logo} />
+      <svg
+        className={styles.logo}
+        width="166"
+        height="195"
+        viewBox="0 0 166 195"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          id={styles.signature}
+          d="M4.95678 4.94806C15.7248 6.76452 31.0401 8.97904 42.7172 11.0042C61.5585 14.2717 80.6786 16.2689 99.6611 18.5098C109.137 19.6283 130.101 21.2652 139.637 21.6077L90.0129 50.2907L23.1658 86.9339M23.1658 86.9339C22.486 80.8447 23.8199 14.0903 23.9376 7.96668M23.1658 86.9339C23.882 93.3487 18.5819 190.079 18.5819 190.079L30.3365 159.583L47.9152 124.336L59.3077 102.793L75.6345 75.0748L82.4096 61.9594L87.6424 51.5901L161.38 103.702L64.2484 94.4048M62.3344 97.6542C97.5 114.5 90.0129 152 87.6424 162C85.272 172 75.6345 194.5 120 190.079"
+          stroke="white"
+          stroke-width="8"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+
       <div className={styles.optionsContainer}>
         <Button
           text="Boards"
